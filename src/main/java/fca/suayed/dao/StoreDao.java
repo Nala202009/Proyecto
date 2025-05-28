c2677227633680d359f68d30da2ec946c22a2dba
Ameyalli López <aameyallilopez@gmail.com>
2025-05-27 23:56:58 -0600

    Update StoreDao.java

--- src/main/java/fca/suayed/dao/StoreDao.java
+++ src/main/java/fca/suayed/dao/StoreDao.java
@@ -18,5 +18,9 @@ public interface StoreDao {
     @SqlUpdate("INSERT INTO productos (nombre, descripcion, precio, cantidad, sku) VALUES(:p.name, :p.description, :p.price, :p.quantity, :p.sku)")
     void addProduct(@BindBean("p") ProductDto productDto);
 
+    @RegisterBeanMapper(ProductDto.class)
+@SqlQuery("SELECT * FROM productos")
+List<ProductDto> getProducts();
+
 
 }
