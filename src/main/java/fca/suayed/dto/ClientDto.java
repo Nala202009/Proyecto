88d7c6fdaad3be3685512462f88843b6df005fd5
Ameyalli López <aameyallilopez@gmail.com>
2025-05-29 00:17:14 -0600

    Update ClientDto.java

--- src/main/java/fca/suayed/dto/ClientDto.java
+++ src/main/java/fca/suayed/dto/ClientDto.java
@@ -1,9 +1,51 @@
 package fca.suayed.dto;
 
 public class ClientDto {
-    public Long id;
-    public String nombre;
-    public String apellido_paterno;
-    public String apellido_materno;
-    public String rfc;
+    private Long id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String rfc;
+
+    // Getters y setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
 }
