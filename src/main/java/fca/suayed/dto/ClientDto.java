f814d7f1669f99ade88ec672dac2e844ec35576f
Ameyalli López <aameyallilopez@gmail.com>
2025-05-28 23:11:46 -0600

    Update ClientDto.java

--- src/main/java/fca/suayed/dto/ClientDto.java
+++ src/main/java/fca/suayed/dto/ClientDto.java
@@ -3,7 +3,7 @@ package org.acme.dto;
 public class ClientDto {
     public Long id;
     public String nombre;
-    public String email;
-
+    public String apellido_paterno;
+    public String apellido_materno;
+    public String rfc;
 }
-
